0e82f84102a160052cf5d92f3230cbebfc517f5f
Nedya Amrih Praksa <nedya@detik.com>
2016-12-05 09:51:37 +0700

    My Permata Wisata Init Commit Android Web View Apps

---
+++ app/src/main/java/com/terpusat/mypermatawisata/HomeActivity.java
@@ -0,0 +1,28 @@
+package com.terpusat.mypermatawisata;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://mypermatawisata.com/opentrip");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+}
