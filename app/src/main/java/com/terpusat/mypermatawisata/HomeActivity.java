cd5e7530895dca55accdbc9ffb945d92db77ee98
Prakasa <prakasa@devetek.com>
2018-07-07 22:19:57 +0700

    read browser history on back press

--- app/src/main/java/com/terpusat/mypermatawisata/HomeActivity.java
+++ app/src/main/java/com/terpusat/mypermatawisata/HomeActivity.java
@@ -1,19 +1,24 @@
 package com.terpusat.mypermatawisata;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.view.KeyEvent;
+import android.app.AlertDialog;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        WebView webView = (WebView) findViewById(R.id.webview);
+        webView = (WebView) findViewById(R.id.webview);
 
         webView.setWebViewClient(new WebViewClient());
 
@@ -21,5 +26,35 @@ public class HomeActivity extends AppCompatActivity {
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    } else {
+                        new AlertDialog.Builder(this)
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .setTitle("Peringatan")
+                                .setMessage("Anda yakin ingin keluar?")
+                                .setPositiveButton("Ya", new DialogInterface.OnClickListener()
+                                {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+                                    }
+
+                                })
+                                .setNegativeButton("Tidak", null)
+                                .show();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
